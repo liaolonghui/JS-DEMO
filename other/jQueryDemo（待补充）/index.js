9e9ed7b92e8a3b8362a65bd09cdf35fa6b15e65a
liaolonghui <1340881930@qq.com>
2020-07-03 11:50:09 +0800

    对DEMO进行前端数据持久化处理

--- other/jQueryDemo（待补充）/index.js
+++ other/jQueryDemo（待补充）/index.js
@@ -1,6 +1,18 @@
 $(function(){
-  //模拟数据
-  var todoList = [];
+  //加载数据的方法
+  function loadDate(){
+    var collection = localStorage.getItem('todo');
+    if(collection){
+      return JSON.parse(collection);
+    }else{
+      return [];
+    }
+  }
+  //保存数据的方法
+  function saveDate(data){
+    localStorage.setItem('todo',JSON.stringify(data));
+  }
+
   //加载网页数据
   load();
   function load(){
@@ -8,6 +20,7 @@ $(function(){
     var doneCount = 0;
     var doneStr = '';
     var todoStr = '';
+    var todoList = loadDate();//获取当前的todoList数据
     if(todoList && todoList.length>0){
       //有数据
       todoList.forEach(function(data,i){
@@ -54,12 +67,15 @@ $(function(){
       if(!val){
         alert('不能为空，请重新输入');
       }else{
-        todoList.unshift({
+        var data = loadDate();      //获取数据再修改数据，最后再保存。
+        data.unshift({
           title: val,
           done: false
         });
         //清空input输入框中的值
         $(this).val('');
+        //保存数据
+        saveDate(data);
         //更新
         load();
       }
@@ -68,13 +84,17 @@ $(function(){
 
   //删除  使用事件代理的方式
   $('#todolist').on('click','a',function(){
-    var i = $(this).parent().children('input').attr('index');
-    todoList.splice(i,1);
+    var i = $(this).parent().children('input[type=checkbox]').attr('index');
+    var data = loadDate();    //删除同理，先获取数据再删除数据，最后再保存数据。
+    data.splice(i,1);
+    saveDate(data);
     load();
   });
   $('#donelist').on('click','a',function(){
-    var i = $(this).parent().children('input').attr('index');
-    todoList.splice(i,1);
+    var i = $(this).parent().children('input[type=checkbox]').attr('index');
+    var data = loadDate();
+    data.splice(i,1);
+    saveDate(data);
     load();
   });
 
@@ -90,15 +110,17 @@ $(function(){
     update(i, 'done', false);
   });
   function update(i, key, value){
-    var todo = todoList.splice(i,1)[0];//此时取到被删除数组的[0]位置处的对象。
+    var data = loadDate();    //更新也是先获取数据再修改数据，最后再保存数据。
+    var todo = data.splice(i,1)[0];//此时取到被删除数组的[0]位置处的对象。
     todo[key] = value;
-    todoList.splice(i,0,todo);
+    data.splice(i,0,todo);
+    saveDate(data);
     load();
   }
 
   //编辑操作
   $('#todolist').on('click','p',function(){
-    var i = $(this).parent().children('input').attr('index');
+    var i = $(this).parent().children('input[type=checkbox]').attr('index');
     var title = $(this).html();
     var $p = $(this);
     $p.html(`
@@ -118,5 +140,5 @@ $(function(){
       }
     });
   });
-  
+
 })
